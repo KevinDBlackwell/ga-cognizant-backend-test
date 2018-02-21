0f8d75d636e5f1bc3baf3f669c3f237b9b551d1a
kevinblackwell414 <kevinblackwell414@gmail.com>
2018-02-21 16:48:04 -0500

    Finished test

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const sqlite = require('sqlite'),
 
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
 
+const sequelize = new Sequelize('database', 'username', null, {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  storage: DB_PATH,
+  }
+);
+
 // START SERVER
 Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
@@ -16,6 +30,60 @@ app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
+  const Recommendation = sequelize.define('recommendation', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true
+    },
+    title: {
+      type: Sequelize.STRING
+    },
+    releaseDate: {
+      type: Sequelize.STRING
+    },
+    genre: {
+      type: Sequelize.STRING
+    },
+    averageRating: {
+      type: Sequelize.INTEGER
+    },
+    reviews: {
+      type: Sequelize.INTEGER
+    }
+  },
+  {
+    freezeTableName: true
+  });
+
+  Recommendation.sync({force: true})
+  .then((req, res) => {
+    return Recommendation
+  })
+
+  Recommendation.findAll({
+    where: {
+      genre: req.data.genre,
+      reviews: {
+        $gte: 5
+      },
+      averageRating: {
+        $gte: 4
+      },
+      releaseDate: {
+        $or: [
+          {$gte: req.data.releaseDate - 15},
+          {$lte: req.data.releaseDate + 15}
+          
+        ]
+      }
+    },
+    order: [
+      ['id', 'ASC']
+    ] 
+  })
+  .then(res => {
+    console.log(res)
+  })
   res.status(500).send('Not Implemented');
 }
 
